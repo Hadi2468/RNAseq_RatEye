dcbfb08d07a7fe33a24b3ea342a0b3b4bfa6ff54
Hadi Hosseini <hosseini@uthsc.com>
2020-03-12 20:40:57 -0500

    update

--- Correlation.R
+++ Correlation.R
@@ -4,15 +4,17 @@
 
 # BiocManager::install("")
 # install.packages("corrplot")
-install.packages("RColorBrewer")
-
-Packages <- c("tximport", "tximportData", "DESeq2", "tidyverse", "dplyr", "vctrs", "fs", "ggplot2", "corrplot", "RColorBrewer")
+# install.packages("RColorBrewer")
+# install.packages("ggpubr")
+# install.packages("pheatmap")
+Packages <- c("tximport", "tximportData", "DESeq2", "tidyverse", "dplyr", "vctrs", "fs", "ggplot2", 
+              "corrplot", "RColorBrewer", "ggpubr", "pheatmap")
 lapply(Packages, library, character.only = TRUE)
-# Packages <- c("vsn", "pheatmap", "ggpubr")
 
 ####################################################
 ### Step 1: Import 45 samples' information files ###
 ####################################################
+
 setwd("./datasets"); getwd()
 samples <- read.csv("samples53.csv", sep=",", header=TRUE)
 dim(samples)
@@ -26,25 +28,33 @@ dim(sub_genes)
 ####################################
 ### Step 2: Correlation Analysis ###
 ####################################
+
 selGenes <- subset(sub_genes, select=c(ENSRNOG00000055293_Ptprb, ENSRNOG00000016696_Angpt2, ENSRNOG00000008587_Tek))
 corrTable <- cbind(sub_samples$Avg_OD, sub_samples$Avg_OS, sub_samples$Avg_IOP, selGenes)    # Correlation tables for three genes 
 names(corrTable) <- c("OD", "OS", "IOP", "PTPRB", "AngPt2", "TEK")
-corrplot(cor(corrTable), method="color", type="lower", order="hclust", col=brewer.pal(n=7, name="PRGn"),
-         addCoef.col = "black", tl.col="black", tl.cex=1)
+corrplot(cor(corrTable), method="color", type="upper", order="FPC", col=brewer.pal(n=7, name="PRGn"),
+         addCoef.col = "black", tl.col="black", tl.cex=1, addrect=3)
+pheatmap(cor(corrTable))
+ggscatter(corrTable, x="OD", y=c("PTPRB", "AngPt2", "TEK"), size = 1, color = "Blue", cor.method="pearson", 
+          combine = TRUE, add="reg.line", conf.int=TRUE, cor.coef=TRUE, xlab="OD", ylab="Normalized gene counts") 
+ggscatter(corrTable, x="OS", y=c("PTPRB", "AngPt2", "TEK"), size = 1, color = "Blue", cor.method="pearson", 
+          combine = TRUE, add="reg.line", conf.int=TRUE, cor.coef=TRUE, xlab="OS", ylab="Normalized gene counts") 
 ggscatter(corrTable, x="IOP", y=c("PTPRB", "AngPt2", "TEK"), size = 1, color = "Blue", cor.method="pearson", 
           combine = TRUE, add="reg.line", conf.int=TRUE, cor.coef=TRUE, xlab="IOP", ylab="Normalized gene counts") 
+ggscatter(corrTable, x="AngPt2", y=c("PTPRB", "TEK"), size = 1, color = "Blue", cor.method="pearson", 
+          combine = TRUE, add="reg.line", conf.int=TRUE, cor.coef=TRUE, xlab="AngPt2", ylab="PTPRB & TEK") 
 ggscatter(corrTable, x="PTPRB", y="TEK", size = 1, color = "Blue", cor.method="pearson", 
           combine = TRUE, add="reg.line", conf.int=TRUE, cor.coef=TRUE, xlab="PTPRB", ylab="TEK") 
-ggscatter(corrTable, x="PTPRB", y="AngPt2", size = 1, color = "Red", cor.method="pearson", 
-          combine = TRUE, add="reg.line", conf.int=TRUE, cor.coef=TRUE, xlab="PTPRB", ylab="AngPt2") 
 
-###########################################
-### Step 6: Heatmap of the count matrix ###
-###########################################
-gene_subset <- c("ENSRNOG00000004712_Angptl1", "ENSRNOG00000055293_Ptprb",
-                 "ENSRNOG00000016696_Angpt2",  "ENSRNOG00000020603_Angptl6",
-                 "ENSRNOG00000020173_Tie1",    "ENSRNOG00000008587_Tek",
-                 "ENSRNOG00000025562_Ang2",    "ENSRNOG00000043032_Ang2")
+
+
+
+
+
+
+########################################
+### Others useful codes for memorize ###
+########################################
 txi.rsem$abundance <- txi.rsem$abundance[gene_subset,]
 txi.rsem$counts <- txi.rsem$counts[gene_subset,]
 txi.rsem$length <- txi.rsem$length[gene_subset,]
@@ -59,10 +69,6 @@ pheatmap(assay(rld)[select,], cluster_rows=FALSE, show_rownames=F, cluster_cols=
 pheatmap(cor(assay(rld)))
 pheatmap(cor(assay(rld)), color=heat.colors, border_color=NA, fontsize=10, fontsize_row=10, height=20, main="Heatmap of sample-to-sample distances")
 hist(assay(rld))
-
-
-
-
 
 samples$OS1[3] <- 0L
 samples$OS1[14] <- 0L
